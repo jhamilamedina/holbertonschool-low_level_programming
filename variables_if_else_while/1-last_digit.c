7c6bef1de30334d53ddfd234d1f6769cb2a4ba13
Maly Medina <jhamilamedina>
2023-08-06 11:02:02 -0700

    Ultimo digito

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -16,11 +16,11 @@ last = n % 10;
 
 if (last > 0)
 {
-printf("Last digit of %d is %d and is greater then 5\n", n, last);
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
 }
 else if (last == 0)
 {
-printf("Last digit of %d is %d and is  0\n", n, last);
+printf("Last digit of %d is %d and is 0\n", n, last);
 }
 else if (last < 6 && last != 0)
 {
