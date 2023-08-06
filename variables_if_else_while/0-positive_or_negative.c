941095fe1063dfd4342691d9535df97d6be93f75
Maly Medina <jhamilamedina>
2023-08-06 07:53:54 -0700

    Numeros positivos o negativos en C

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
-#include <stdlib.h> 
+#include <stdlib.h>
+
 #include <time.h>
 #include <stdio.h>
 
@@ -11,7 +12,6 @@
 int main(void)
 {
 int n;
-	
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
@@ -19,14 +19,13 @@ if (n > 0)
 {
 printf("%d is positive\n", n);
 }
-else if (n ==0)
+else if (n == 0)
 {
 printf("%d is zero\n", n);
 }
 else
 {
 printf("%d is negative\n", n);
-}	 
-/* your code goes there */
+}
 return (0);
 }
