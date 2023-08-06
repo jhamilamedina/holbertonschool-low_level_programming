97649bd2ac90f2e4f527c3afb31a0876fc43e4ce
Maly Medina <jhamilamedina>
2023-08-06 12:49:56 -0700

    El alfabeto en mayus y minus en C

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,8 +3,8 @@
 #include <time.h>
 
 /**
- * main - prints  the alphabet in lowercase, and then in 
- * uppercase, followed by a new line.
+ * main - prints the alphabet in
+ * lowercase, followed by a new line.
  * Return: Always 0 (Success)
  */
 
