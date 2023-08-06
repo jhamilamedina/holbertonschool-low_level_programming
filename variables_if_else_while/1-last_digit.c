0f0651022afdecac4f478bdb6e71bd3d8d376441
Maly Medina <jhamilamedina>
2023-08-06 10:20:49 -0700

    Asignar un número aleatorio a la variable n

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main -prints a text according number
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{int n, lastd;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastd = n % 10;
+
+if (lastd > 0)
+{
+printf("Last digit of %d is %d and is greater then 5\n", n, lastd);
+}
+else if (lastd == 0)
+{
+printf("Lastd digit of %d is %d and is  0\n", n, lastd);
+}
+else if (lastd < 6 && lastd != 0)
+{
+printf("Lastd digit of %d is %d and is less than 6 and 0\n", n, lastd);
+}
+/* your code goes there */
+return (0);
+}
