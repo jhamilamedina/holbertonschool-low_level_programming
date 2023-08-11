f12a7d99abfc2bfc8aef2e9fcac4887ac2a96b32
Maly Medina <jhamilamedina>
2023-08-11 09:14:00 -0700

    C: Imprimir el valor absoluto de un nro(bucles anidados)

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ * @c: The number to be computed.
+ * Return: Absolute valor of number or zero
+ */
+
+int _abs(int c)
+{
+
+if (c < 0)
+{
+int abs_val;
+abs_val = -1;
+return (abs_val);
+}
+return (c);
+}
