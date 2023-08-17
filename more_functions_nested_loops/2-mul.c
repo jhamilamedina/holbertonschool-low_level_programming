c3b8ad741eeb0de2cc7f978f214f40176ea28627
Maly Medina <jhamilamedina>
2023-08-16 17:26:50 -0700

    C: Multiplicacion de 2 nros enteros

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers.
+ * @a: the first number to be computed
+ * @b: the second number to be computed
+ * Return: Integer value
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
