d012b506d8d6ac043c747000623e98dcc3e618a3
Maly Medina <jhamilamedina>
2023-08-16 22:26:49 -0700

    C: Imprimir los numeros del 0 al 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+
+void print_numbers(void)
+{
+
+int d;
+
+for (d = '0'; d <= '9'; d++)
+_putchar(d);
+_putchar('\n');
+}
