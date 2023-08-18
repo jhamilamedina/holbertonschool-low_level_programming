883409b83e3be9bfb8dff6c96f0a79af12a01c43
Maly Medina <jhamilamedina>
2023-08-17 22:40:47 -0700

    C: Dibujo de 2 lineas diagonales

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagonal - draws a diagonal line.
+ * @n: No of time
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+int x, y;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < n; x++)
+{
+for (y = 0; y < x; y++)
+{
+_putchar(32);
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+}
+
