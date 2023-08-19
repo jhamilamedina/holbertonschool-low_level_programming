b5def7a4989c93442270ba898f1f8cb2c2411d7b
Maly Medina <jhamilamedina>
2023-08-18 17:23:58 -0700

    C: Imprimir 2 cuadrados tamaños(2x2)(10x10)

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square.
+ * @size: The number of square/ number of time
+ * Return: Empty
+ */
+
+void print_square(int size)
+{
+int x, y;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+}
