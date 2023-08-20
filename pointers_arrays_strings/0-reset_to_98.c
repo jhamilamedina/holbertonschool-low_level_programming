596e0402a81dc239121753c00e8ed568734a5a24
Maly Medina <jhamilamedina>
2023-08-20 00:06:56 -0700

    C:Cambio de valor *n=402 a *n=98(pointer)

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value it points to 98
+ * @n: Pointer to update
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+
+*n = 98;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef __M_H__
 #define __M_H__
 int _putchar(char c);
+void reset_to_98(int *n);
 int _strlen(char *s);
 #endif 
