90eee5c13b081c7994deabf4bfdeef75d4a402dd
Maly Medina <jhamilamedina>
2023-08-28 16:42:17 -0700

    C: Funcion qu concatena 2 cadenas(string)

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+
 /**
  * str_concat - function tha concatenates two strings
  * @s1: first string
@@ -11,25 +12,38 @@
 
 char *str_concat(char *s1, char *s2)
 {
-int len1, len2;
-char *s;
+int i = 0, j = 0, k = 0, l = 0;
+	char *s;
 
-if (s1 == NULL)
-{
-s1 = "";
-}
+	if (s1 == NULL)
+		s1 = "";
 
-if (s2 == NULL)
-{
-s2 = "";
-}
-len1 = strlen(s1);
-len2 = strlen(s2);
+	if (s2 == NULL)
+		s2 = "";
 
-s = malloc(len1 + len2 + 1);
-if (s == NULL)
-{
-return (NULL);
-}
-return (0);
+	while (s1[i])
+		i++;
+
+	while (s2[j])
+		j++;
+
+	l = i + j;
+	s = malloc((sizeof(char) * l) + 1);
+	if (s == NULL)
+		return (NULL);
+	j = 0;
+	while (k < l)
+	{
+		if (k <= i)
+			s[k] = s1[k];
+
+		if (k >= i)
+		{
+			s[k] = s2[j];
+			j++;
+		}
+		k++;
+	}
+	s[k] = '\0';
+	return (s);
 }
