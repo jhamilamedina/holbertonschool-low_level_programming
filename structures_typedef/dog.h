bf2a6ef0acc3b03b13382c2ae152de52e79e496a
Maly Medina <jhamilamedina>
2023-08-30 23:19:31 -0700

    C Imprimir nuevo nombre dog_t para struct_dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef __DOG_H__
 #define __DOG_H__
-
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * struct dog - Structut for created a dog
  * @name: name of dog
@@ -17,4 +19,18 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+/**
+ * struct dog_t - new name for the type struct dog
+ * @name: name of dog
+ * @age: age a dog
+ * @owner: owner of dog
+ */
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 #endif
